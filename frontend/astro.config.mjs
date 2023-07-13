65b955887324bb0f6ca5bf6d63356c99603fba6a
Adam Griswold <adam.griswold@nucor.com>
2023-07-13 21:40:11 +0000

    feat: init frontend

---
+++ frontend/astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config'
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite'
+import tailwind from '@astrojs/tailwind'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+const env = loadEnv('', process.cwd(), 'STORYBLOK')
+
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      apiOptions: {
+        region: 'us',
+      },
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+      },
+    }),
+    tailwind(),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+})
